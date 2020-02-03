cf13fae57da2f8c6bac88602d48a69acca0a7750
tzur.almog@gmail.com <tzur.almog@gmail.com>
2020-02-03 12:29:13 -0500

    Added sig hadler

--- A1/Assign1.cpp
+++ A1/Assign1.cpp
@@ -1,9 +1,4 @@
-//
-// Help file for cs305 Assignment 7
-//
-// This file has some code any many comments to
-// direct you through assignment #7
-//
+// Tzurs Assignment 1
 
 #include <sys/types.h>
 #include <unistd.h>
@@ -11,15 +6,36 @@
 #include <stdio.h>
 #include <errno.h>
 #include <stdlib.h>
+#include <signal.h>
 #include <sys/resource.h>
 
-// need just a main function, no args
+int counter = 0, counter_2G = 0;
+
+
+typedef union{
+    int exit_status;
+    struct{
+        unsigned  sig_num:7;
+        unsigned  core_dmp:1;
+        unsigned  exit_num:8;
+    }parts;
+}LE_Wait_Status;
+
+void sig_handler(int signal){
+	printf("\n\nCHILD PROC: Awake in handler - You Got Me With Signal\n \
+Number %d after spinning for more\n than %d %s loop iterations\n\n", 
+                signal, ((counter_2G)?counter_2G*2:counter),
+                ((counter_2G)?"Billion":"\0"));
+
+    printf("Loading prof program");
+    fflush(stdout);
+	exit(1);
+}
 
 int main(void)
 {
 
-    // some local variables
-
+    //Local variables
     pid_t pid, ppid;
     int ruid, rgid, euid, egid;
     int priority;
@@ -54,13 +70,11 @@ PARENT PROC:  Effective GID is:\t\t%d\n\
 PARENT PROC:  Process priority is:\t%d\n",
            pid, ppid, ruid, rgid, euid, egid, priority);
 
-    printf("\nPARENT PROC: will now create child, write pipe,\n \
-and do a normal termination\n");
 
-    // now use the fork() call to create the child:
-    //
-    // format is:
-    //
+/*
+    Fork to create child
+*/
+
     switch (pid = fork())
     {
     case -1: // if the call failes
@@ -68,18 +82,33 @@ and do a normal termination\n");
         exit(4);
 
     default: // this is the parent's case
-             // parent must write message to pipe and
-             // do a normal exit
         printf("\nPARENT PROG: created Child with %d PID\n",pid);
-        read(sync_pipe[0], msg_buf, 10);
-        printf("\nPARENT PROG: Succsess\n");
         break;
 
-    case 0: // this is the child's case
-            // child must create and print report
-            // child must read pipe message and print
-            // a modified version of it to output
-            // child must do a normal exit
+    case 0: // child case
+
+        // gets mask from system
+        sigset_t	mask, proc_mask;
+        // makes new handler
+	    struct sigaction sigHandler;
+        // LE_Wait_Status exit_union;
+
+        // Empties previous mask
+        sigemptyset(&proc_mask);
+	    sigprocmask(SIG_SETMASK, &proc_mask, NULL);
+
+	
+	    sigemptyset(&mask);
+	    sigHandler.sa_mask = mask;
+	    sigHandler.sa_handler = sig_handler;
+	    sigHandler.sa_flags = 0;
+
+	    if(sigaction(SIGTERM, &sigHandler, NULL) == -1){
+	    	perror("failed in sigaction:");
+	    	exit(2);
+	    }
+
+        //  print information
         printf("\nThis is the Child process report:\n");
         pid = getpid();
         ppid = getppid();
@@ -98,12 +127,24 @@ CHILD PROC:  Effective GID is:\t\t%d\n\
 CHILD PROC:  Process priority is:\t%d\n",
                pid, ppid, ruid, rgid, euid, egid, priority);
 
-        //---------------------------------
-    write(sync_pipe[1], "x", 10);
+        // write to parent
+        write(sync_pipe[1], "x", 10);
 
-        printf("CHILD PROC: Process parent ID now is:   %d\n",
-               getppid());
+        // enter endless loop and wait for parent to terminate
+        while (true){
+		    counter ++;
+		    if (counter < 0){
+			counter = 0;
+			counter_2G++;
+		    }
+	    }
     } // switch and child end
+
+    
+    read(sync_pipe[0], msg_buf, 10);
+    printf("\nPARENT PROG: Killing child\n");
+    kill(pid, SIGTERM);
+        
 
 
     exit(0);
--- A1/Hw7.cpp
+++ A1/Hw7.cpp
@@ -15,7 +15,7 @@
 #include <stdlib.h>
 #include <sys/resource.h>
 
-// need just a main function, no args
+
 
 int main(void)
 {
